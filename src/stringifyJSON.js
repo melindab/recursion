bce931c3fa5a6ea63e5f21f8909f3ec05f846f95
melindab <melindab@users.noreply.github.com>
2015-07-30 23:33:36 -0500

    Implement stringifyJSON. All tests pass for basic requirements.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,52 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var items = [];
+  var item = '';
+  var str = '';
+
+  if (typeof obj === 'undefined' || typeof obj === 'null' || typeof obj === 'symbol') {
+    return;
+  }
+
+  if (typeof obj === 'function') {
+    return;
+  }
+
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return '' + obj + '';
+  }
+  
+  if (Array.isArray(obj)) {
+    for (var i = 0; i < obj.length; i++) {
+      item = stringifyJSON(obj[i]);
+      items.push(item);
+    }
+    
+    str = items.join(',');
+
+    return '[' + str + ']';
+  }
+
+  if (typeof obj === 'object') {
+    if (obj === null) {
+      return 'null';
+    }
+
+    for (var prop in obj) {
+      if (obj.hasOwnProperty(prop) && typeof obj[prop] !== 'function' && typeof obj[prop] !== 'undefined') {
+        item = stringifyJSON(prop) + ':' + stringifyJSON(obj[prop]);
+        items.push(item);
+      }
+    }
+
+    str = items.join(',');
+
+    return '{' + str + '}';
+  }
+
 };
